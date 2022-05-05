10bb46d1e7f06efc92ee6bfddc5cbe5ac792177b
Richard <isit_ric@hotmail.com>
2022-05-05 13:19:13 +0100

    ricCommit2

--- littleLeaders/littleLeaders.js
+++ littleLeaders/littleLeaders.js
@@ -1,4 +1,6 @@
 // get element and event listeners here
+// const btnDonald = document.getElementById("donaldBtn"); //Player 1 Button
+// const btnElon = document.getElementById("elonBtn"); //Player 1 Button
 
 // Parent class
 class leader {
@@ -31,43 +33,121 @@ class musk extends leader {
     }
 }
 
-const donald = new trump("Donald");
-const elon = new musk("Elon");
+// function srtDonald (){
+//     const donald = new trump("Donald");
+//     donald = true;
+//     }
 
-//Button functions
-function goSocial(){
-    randScore = Math.floor(Math.random()*10);
-    newRand = donald._narc += randScore;
-    console.log(`newRand = ${newRand}`);
-    return newRand;
-}
+// btnDonald.addEventListener("click", leaderSel)
+// btnElon.addEventListener("click", leaderSel)
 
-// Win/Loss conditions
-function winCond() {
-    if (leader._narc >= 30) {
-        console.log("you win!");
+// const leaderSel = (e) => {
+//     debugger;
+//     this.app={};
+//     if (e.path === "donaldBtn") {
+//         this.app.donald = new trump("Donald");
+//     } 
+//     if (e.path === "elonBtn") {
+//         this.app.elon = new musk("Elon");
+//     }
+// }
+
+leaderSel("click");
+
+// what happens idf smae char selected twice/
+
+
+
+    // BUTTON - TWEET
+    const goSocial = () => {
+        //Need to also include negative modifier for insec
+        randScore = Math.floor(Math.random() * 6);
+        if (elon) {
+            newRand = elon._narc += randScore;
+            console.log(`elon newRand = ${newRand}`);
+            // put in something for insecurity - slightly less than the narc result
+        } // How to do if statement on donald when the instance is not created?
+        // }
+        //  if (donald) {
+        //     newRand = donald._narc += randScore;
+        //     console.log(`donald newRand = ${newRand}`);
+        // }
+        return newRand;
     }
-}
 
-function loseCond() {
-    if (leader._insec >= 30) {
-        console.log("you lose!");
+    // BUTTON - PRESS RELEASE
+    const goPress = () =>{
+        randScore = Math.floor(Math.random() * 10);
+        if (elon) {
+            newRand = elon._insec += randScore;
+            console.log(`elon newRand = ${newRand}`);
+        }
+        return newRand;
     }
-}
 
-//Turn counter
-function turnCount() {
-    while (leader._insec < 30 || leader._narc < 30) {
-        turn = 0;
-        turn += 1;
-        winCond();
-        loseCond();
-        return turn;
+    // UNIQUE BUTTON - TRUMP - how to load these into UI???
+    const goControv = (_controv) =>{
+        //Generate controversy animation & info
+        // Put limits on this. FOr example can only be used once every 3 turns
+        if (donald._controv > 30) {
+            loseCond();
+        } else {
+            donald._controv += Math.floor(Math.random() * 6);
+            donald._narc += Math.floor(Math.random() * 6);
+            donald._insec -= Math.floor(Math.random() * 6);
+        }
     }
-}
+
+    // UNIQUE BUTTON - ELON - how to load these into UI???
+   const goIntox = (_intox) =>{}
 
 
-goSocial();
-console.log(donald);
-// console.log(elon);
+    // Win/Loss conditions
+    const winCond = (win) => {
+        if (leader._narc >= 30) {
+            console.log("you win!");
+        } //Should we also win if insec reaches 0?
+    }
 
+    const loseCond = (lose) => {
+        if (leader._insec >= 30) {
+            console.log("you lose!");
+        } //Should we also lose if narc reaches 0?
+    }
+
+    // //Turn counter - OR USE A TIMER!!!
+    // const turnCount = () => {
+    //     while (leader._insec < 30 || leader._narc < 30) {
+    //         turn = 0;
+    //         turn += 1;
+    //         winCond();
+    //         loseCond();
+    //         return turn;
+    //     } //consider including negative impact on player scores so that eventually the player loses if they do not do anything
+    // }
+
+
+    goSocial();
+    console.log(elon);
+
+
+    // turn function which checks win/loss conditions then calls on player function (which calls on turn function after button oressed)
+    // For all buttons to work simultaniously we need to integrate the JS with a front end.
+    // Buttons with addEvent listeners
+    // getElement for narc and insec categories to be displayed on-screen
+    // Basic html - no need for styling right now
+    // determine impact of buttons on scores
+    // determine impact of intox and controv characteristics
+    // How to load unique buttons into UI?
+
+
+    // Timer?? with associated graphics (i.e. rogan show, police, rockets etc)
+
+    // gameturn
+    // check win conditions
+    // call reduce stats function
+    // call player function
+
+    // player
+    // press GamepadButton
+    // call gameturn function
