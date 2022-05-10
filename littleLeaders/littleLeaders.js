3d98c6d01893af770932726481a66c85f7c7c536
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-05-10 22:40:04 +0100

    Recent Changes, Working
    Needs a Cap on the Insecurity. Added new images. Added Polaroids. Added Styling.

--- littleLeaders/littleLeaders.js
+++ littleLeaders/littleLeaders.js
@@ -1,3 +1,132 @@
+
+const charConfirm = document.getElementById("charConfirm"); // Character confirmation
+
+ // Character Movement For Reasons!
+const elonMain = document.querySelector(".l1");
+const elonHead = document.querySelector(".leader-1-head");
+const elonTorso = document.querySelector(".leader-1-torso");
+const elonLArm = document.querySelector(".leader-1-larm");
+const elonRArm = document.querySelector(".leader-1-rarm");
+const elonLLeg = document.querySelector(".leader-1-lleg");
+const elonRLeg = document.querySelector(".leader-1-rleg");
+
+const donaldMain = document.querySelector(".l2");
+const donaldHead = document.querySelector(".leader-2-head");
+const donaldTorso = document.querySelector(".leader-2-torso");
+const donaldLArm = document.querySelector(".leader-2-larm");
+const donaldRArm = document.querySelector(".leader-2-rarm");
+const donaldLLeg = document.querySelector(".leader-2-lleg");
+const donaldRLeg = document.querySelector(".leader-2-rleg");
+
+///////////////////////////////////////////////// CHARACTER CREATION //////////////////////////////////////////////////////////
+
+// Dom's > DOM TESTING
+function elonWave() {
+    selElon.addEventListener("mouseover", () => {
+        elonLArm.style.transform = "rotate(120deg)";
+        elonLArm.style.top = "-50px";
+    })
+
+    selElon.addEventListener("mouseout", () => {
+        elonLArm.style.transform = "rotate(0deg)";
+        elonLArm.style.top = "50px";
+    })
+}  
+
+function donaldWave() {
+    selDonald.addEventListener("mouseover", () => {
+        donaldLArm.style.transform = "rotate(120deg)";
+        donaldLArm.style.top = "-50px";
+    })
+
+    selDonald.addEventListener("mouseout", () => {
+        donaldLArm.style.transform = "rotate(0deg)";
+        donaldLArm.style.top = "50px";
+    })
+}
+
+
+////////////////////////////////////////////////// DONALD IMAGES/POLAROIDS /////////////////////////////////////////////////////////
+
+const polaroidOne = document.getElementById("masonry-container-1");
+
+
+function donaldPic1() {
+    socialBtn.addEventListener("click", () => {
+        polaroidOne.style.transform = "rotate(1050deg)";
+        polaroidOne.style.top = "50%";
+        polaroidOne.style.opacity = "100%";
+        setTimeout(() => {
+            polaroidOne.style.transform = "rotate(50deg)";
+            polaroidOne.style.top = "100%";
+            polaroidOne.style.opacity = "0%";
+        }, 3000);
+
+    })
+}
+
+const polaroidTwo = document.getElementById("masonry-container-2");
+
+
+function donaldPic2() {
+    pressBtn.addEventListener("click", () => {
+        polaroidTwo.style.transform = "rotate(1050deg)";
+        polaroidTwo.style.top = "50%";
+        polaroidTwo.style.opacity = "100%";
+        setTimeout(() => {
+            polaroidTwo.style.transform = "rotate(50deg)";
+            polaroidTwo.style.top = "100%";
+            polaroidTwo.style.opacity = "0%";
+        }, 3000);
+
+    })
+}
+
+const polaroidThree = document.getElementById("masonry-container-3");
+
+
+function donaldPic3() {
+    controvBtn.addEventListener("click", () => {
+        polaroidThree.style.transform = "rotate(1050deg)";
+        polaroidThree.style.top = "50%";
+        polaroidThree.style.opacity = "100%";
+        setTimeout(() => {
+            polaroidThree.style.transform = "rotate(50deg)";
+            polaroidThree.style.top = "100%";
+            polaroidThree.style.opacity = "0%";
+        }, 3000);
+
+    })
+}
+
+const polaroidWin = document.getElementById("masonry-container-win");
+
+
+function donaldPicWin() {
+        polaroidWin.style.transform = "rotate(1050deg)";
+        polaroidWin.style.top = "50%";
+        polaroidWin.style.opacity = "100%";
+        setTimeout(() => {
+            polaroidWin.style.transform = "rotate(50deg)";
+            polaroidWin.style.top = "100%";
+            polaroidWin.style.opacity = "0%";
+        }, 3000);
+}
+
+const polaroidLose = document.getElementById("masonry-container-lose");
+
+
+function donaldPicLose() {
+        polaroidLose.style.transform = "rotate(1050deg)";
+        polaroidLose.style.top = "50%";
+        polaroidLose.style.opacity = "100%";
+        setTimeout(() => {
+            polaroidLose.style.transform = "rotate(50deg)";
+            polaroidLose.style.top = "100%";
+            polaroidLose.style.opacity = "0%";
+        }, 3000);
+}
+
 ////////////////////////////////////////////////// DOM REFERENCES & VARIABLES /////////////////////////////////////////////////////////
 
 // CHARACTER SELECTION INTEGRATION
@@ -11,12 +140,20 @@ const goPressBtn = document.getElementById("pressBtn");
 const goControvBtn = document.getElementById("controvBtn");
 // const goIntoxBtn = document.getElementById("intoxBtn");
 
+// TRIGGER BUTTON FEEDBACK
+let youWin = document.getElementById(`leader`);
+let youLose = document.getElementById(`leader`);
+let youSocial = document.getElementById(`leader`);
+let youPress = document.getElementById(`leader`);
+let youControv = document.getElementById(`leader`);
+
 // CHARACTER HEALTH BARS INTEGRATION
 let statsNarc = document.getElementById("statsNarc"); // Narcissm stat
 let statsInsec = document.getElementById("statsInsec"); // Insecurity stat
 let statsControv = document.getElementById("statsControv"); // Controversy stat
 // const statsIntox = document.getElementById("statsIntox"); // Intoxication stat
-const winLoseDisplay = document.getElementById("winLose")
+
+
 
 // VARIABLES TO HIDE CHARACTERS
 // let hideDon = document.getElementById("donald"); //Hide the <div> containing Donald
@@ -39,9 +176,9 @@ class trump extends leader {
     constructor(name) {
         super(name);
         this._name = "Donald";
-        this._narc = 15;
-        this._insec = 15;
-        this._controv = 15;
+        this._narc = 0;
+        this._insec = 0;
+        this._controv = 0;
     }
 }
 
@@ -50,9 +187,9 @@ class musk extends leader {
     constructor(name) {
         super(name);
         this._name = "Elon";
-        this._narc = 10;
-        this._insec = 5;
-        this._intox = 20;
+        this._narc = 0;
+        this._insec = 0;
+        this._intox = 0;
     }
 }
 
@@ -75,8 +212,6 @@ const hideE = () => {
     // }
     // hideDon.style.display = "flex";
 }
-
-
 
 // Character selection event listeners - to trigger srtDonald and srtElon functions
 // selDonald.addEventListener("click", srtDonald)
@@ -114,32 +249,29 @@ elonBool = 1;
 //     hideD()
 // }
 
-//////////////////////////////////////////////// WIN/LOSS CONDITIONS //////////////////////////////////////////////////////
+//////////////////////////////////////////////// WIN/LOSS CONDITIONS & POINTS CAP //////////////////////////////////////////////////////
 
 // Combined win/loss function
 const winLose = () => {
-    if (donald._narc >= 300 || elon._narc >= 30) {
+    if (donald._narc >= 100 || elon._narc >= 100) {
+        donald._narc = 100;
         console.log("you win!");
-       
+        donaldPicWin()
     }
 
-    if (donald._insec >= 300 || elon._insec >= 30) {
+    if (donald._insec >= 100 || elon._insec >= 100) {
+        donald._insec = 100;
         console.log("you lose!");
-       
+        donadonaldPicWin()
     }
 }
 
 //////////////////////////////////////////////// BUTTONS ///////////////////////////////////////////////////////////////
 
-
-// RANDOM NO GENERATOR 
-
-
 // BUTTON - TWEET
 function goSocial() {
     if (donaldBool == 1) {
         randScore = Math.floor(Math.random() * 6);
-            console.log(`Randscore is: ${randScore}`);
         newRand = donald._narc + randScore;
         statsNarc.innerHTML = donald._narc;
         statsInsec.innerHTML = randScore/2;
@@ -165,6 +297,7 @@ const goPress = () => {
         donald._insec += randScore;
         statsInsec.innerHTML = newRand;
         statsNarc.innerHTML = randScore/2;
+        youPress = pPress;
         winLose();
         return newRand;
     } else if (elonBool == 1) {
@@ -178,8 +311,8 @@ const goPress = () => {
     }
 }
 
-// UNIQUE BUTTON - TRUMP - how to load these into UI???
-// ///////// controversy updated button - with click counter 
+// UNIQUE BUTTON - TRUMP
+//controversy updated button - with click counter 
 let clicks = 1 
 const clickCounter = document.getElementById("controvClicks")
 const disablebtn = document.getElementById("controvBtn").disabled = false;
@@ -204,7 +337,6 @@ const goControv = (_controv) => {
         controvClicks.innerHTML = "No more controversy available!";
         disablebtn = true;
     }
-
     console.log (clicks)
 }
 
@@ -215,7 +347,6 @@ let goIntox = () => {
         newRand = elon._narc + randScore;
         statsNarc.innerHTML = newRand;
         elon._narc += newRand;
-    
         winLose();
         return newRand;
     }
@@ -223,7 +354,7 @@ let goIntox = () => {
 
 goSocialBtn.addEventListener("click", goSocial);
 goPressBtn.addEventListener("click",goPress);
-goControvBtn.addEventListener("click",  goControv);
+goControvBtn.addEventListener("click",goControv);
 // goIntoxBtn.addEventListener("click", goIntox);
 
 
